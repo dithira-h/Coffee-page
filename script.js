5fc2124287288b5dbe84d10c894dfc4f7f86e689
Dithira Hettiararchchi <66258991+dithira-h@users.noreply.github.com>
2024-06-22 01:44:37 +0000

    w

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+
 document.getElementById('getstarted').addEventListener('click', function() {
     const text = document.getElementById('emailad').value;
     console.log(text);
+    if (text === 'dithira@gmail.com'){
+      window.open('guest.html', '_self');
+      console.log('Welcome to Dithira\'s insights');
+    }
 });

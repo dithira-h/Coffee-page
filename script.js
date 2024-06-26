6205a35cc044f8b9bc387be17f4ad63ad2242cf5
Dithira Hettiararchchi <66258991+dithira-h@users.noreply.github.com>
2024-06-26 20:38:55 +0000

    update 2

--- script.js
+++ script.js
@@ -6,4 +6,10 @@ document.getElementById('getstarted').addEventListener('click', function() {
       window.open('guest.html', '_self');
       console.log('Welcome to Dithira\'s insights');
     }
+    else if (typeof text === 'string'){
+      window.open('welcome.html', '_self');
+      console.log("loaded");
+    }
 });
+
+

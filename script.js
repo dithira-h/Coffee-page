c47091ef4d64a39e400eae924215e0375816263d
Dithira Hettiararchchi <66258991+dithira-h@users.noreply.github.com>
2024-06-27 20:09:08 +0000

    backend

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 
+
 document.getElementById('getstarted').addEventListener('click', function() {
     const text = document.getElementById('emailad').value;
     console.log(text);
-    if (text === 'smd@gmail.com'){
-      window.open('pages/guest.html', '_self');
-      console.log('Welcome to Dithira\'s insights');
-    }
-    else if (typeof text === 'string'){
+    if (text === 'coffee'){
       window.open('pages/welcome.html', '_self');
-      console.log("loaded");
+  
+    } else if (text === '123'){
+        alert("Please re-enter your email");
+    } else {
+        window.open('pages/guest.html', '_self');
+        alert("you don't have access to this page" + text);
+        Error("Invalid email");
     }
 });
 
